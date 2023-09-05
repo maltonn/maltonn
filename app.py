69261708d03a97e21e1f9f4d8759543772ab8917
Maltonn <ymatsubayashi09@outlook.jp>
2023-09-05 14:53:28 +0900

    init

---
+++ app.py
@@ -0,0 +1,24 @@
+import json
+from flask import Flask, jsonify, render_template
+
+app = Flask(__name__)
+
+# http://127.0.0.1:5000をルートとして、("")の中でアクセスポイント指定
+# @app.route("hoge")などで指定すると、http://127.0.0.1:5000/hogeでの動作を記述できる。
+
+
+@app.route("/")
+def root():
+    return render_template('index.html')
+
+
+@app.route("/bc")  # buisiness card
+def bc():
+    with open("bc.json", encoding="utf-8_sig") as f:
+        data = json.load(f)
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    # webサーバー立ち上げ
+    app.run(debug=True)
